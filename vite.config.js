b6de9163e37737ab06663926c3f8e1c11fe6f425
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-07-11 10:44:28 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // <--- required to listen on all network interfaces
+    port: 5173, // optional, default is 5173
+  },
 });
